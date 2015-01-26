3527ef0e68116734667a58c410c91e866c87b0a7
MoreThanOneAnimal <morethanoneanimal@gmail.com>
2015-01-26 02:59:59 +0100

    a couple of test for simulationManager

--- simulationManager.py
+++ simulationManager.py
@@ -27,7 +27,8 @@ class SimulationManager:
                 pygame.K_ESCAPE: self.__exit,
                 pygame.K_SPACE:  self.__pauseSwitch,
                 pygame.K_m: self.__speedUp,
-                pygame.K_n: self.__speedDown
+                pygame.K_n: self.__speedDown,
+                pygame.K_s: self.__oneStepForward
         }[key]()
 
     def isStopped(self):
@@ -37,5 +38,8 @@ class SimulationManager:
     def __pauseSwitch(self):
         self.timeFactor, self.prevTimeFactor = self.prevTimeFactor, self.timeFactor
     def __speedUp(self): self.timeFactor = min(8, self.timeFactor*2)
-    def __speedDown(self): self.timeFactor = max(1/8, self.timeFactor*2)
+    def __speedDown(self): self.timeFactor = max(1/8, self.timeFactor/2)
+    def __oneStepForward(self):
+        if self.isStopped(): self.makeStep()
+        else: print("Can't make step: simulation is running")
 
--- test/test_simulation_manager.py
+++ test/test_simulation_manager.py
@@ -7,6 +7,10 @@ class TestSimulationManager(unittest.TestCase):
         self.mock = Mock()
         self.simulation = SimulationManager(self.mock)
 
+    def __setStopped(self, val):
+        if self.simulation.isStopped() != val: self.simulation.processKey(pygame.K_SPACE)
+        self.assertEqual(val, self.simulation.isStopped())
+
     def test_makeSteps(self):
         mock = Mock()
         s = SimulationManager(mock)
@@ -26,6 +30,21 @@ class TestSimulationManager(unittest.TestCase):
         for x in range(100): self.simulation.update(config.updateFrame)
         self.assertTrue(self.mock.update.call_count > 0)
         self.assertEqual(100, self.mock.update.call_count)
+
+    def test_s_key(self):
+        self.__setStopped(False)
+        self.mock.reset_mock()
+        self.simulation.processKey(pygame.K_n)
+        self.assertEqual(0, self.mock.update.call_count )
+        self.simulation.processKey(pygame.K_SPACE)
+        self.simulation.processKey(pygame.K_n)
+        self.assertTrue( self.mock.update.called_once_with() )
+
+    def test_n_key(self):
+        """slowing down"""
+        self.__setStopped(False)
+        for x in range(20): self.simulation.processKey(pygame.K_n)
+        self.assertEqual(1/8, self.simulation.timeFactor)
 
     def test_escape_key(self):
         self.simulation.processKey(pygame.K_ESCAPE)
