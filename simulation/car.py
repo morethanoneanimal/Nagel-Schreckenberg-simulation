2773bcf5db0391589c6b867bcd0e8df37c8e004b
MoreThanOneAnimal <morethanoneanimal@gmail.com>
2015-01-27 02:49:43 +0100

    car refactoring + tests

--- simulation/car.py
+++ simulation/car.py
@@ -7,14 +7,13 @@ class Car:
         self.pos = pos
 
     def update(self):
-        self.velocity = self.velocity + 1
-        self.velocity += 1
-        # checking distance
-        distance = self.road.distanceToNextThing(self.pos)
-        self.velocity = min(self.velocity, distance, self.road.getSpeedLimitAt(self.pos))
+        self.velocity = self.calcNewVelocity()
 
         if self.velocity > 0 and random.random() >= 0.75:
             self.velocity -= 1
 
         self.pos = self.pos[0] + self.velocity, self.pos[1]
         return self.pos
+
+    def calcNewVelocity(self):
+        return min(self.velocity + 1, self.road.getMaxSpeedAt(self.pos))
---
+++ test/test_car.py
@@ -0,0 +1,26 @@
+import unittest, config, random
+from simulation.road import Road
+from simulation.car import Car
+from simulation.speedLimits import SpeedLimits
+
+class TestCar(unittest.TestCase):
+    def setUp(self):
+        random.seed(None)
+        speedLimits = SpeedLimits( [ (((50, 0), (99, 0)), 2, 0, True) ] )
+        road = self.road = Road(1, 100, speedLimits)
+        self.car1 = Car(road, (0, 0))
+        self.carA = Car(road, (25, 0))
+        self.carB = Car(road, (26, 0))
+        self.carC = Car(road, (28, 0))
+        self.car2 = Car(road, (50, 0))
+        self.allCars = [self.car1, self.carA, self.carB, self.carC, self.car2]
+        self.road.placeObjects( self.allCars )
+
+    def test_assumptions(self):
+        for car in self.allCars:
+            car.velocity = random.randint(-100, 100)
+            newVelocity = car.calcNewVelocity()
+            self.assertTrue( newVelocity <= self.road.distanceToNextThing(car.pos) )
+            self.assertTrue( newVelocity <= self.road.getSpeedLimitAt(car.pos) )
+            self.assertTrue( newVelocity <= config.maxSpeed )
+
