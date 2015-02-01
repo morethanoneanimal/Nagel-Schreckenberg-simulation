d1f51031c617b412bc8185f64ee22ac7339b0400
MoreThanOneAnimal <morethanoneanimal@gmail.com>
2015-02-01 13:20:14 +0100

    SimpleTrafficGenerator can get an extra parameter

--- simulation/trafficGenerators.py
+++ simulation/trafficGenerators.py
@@ -1,10 +1,11 @@
 import random
 
 class SimpleTrafficGenerator():
-    def __init__(self):
+    def __init__(self, carPerUpdate=1):
         self.queue = 0
+        self.carPerUpdate = carPerUpdate
     def generate(self, road):
-        amount = random.randint(0, 2)
+        amount = random.randint(0, self.carPerUpdate)
         self.tryGenerate(road, amount)
 
     def tryGenerate(self, road, amount):
